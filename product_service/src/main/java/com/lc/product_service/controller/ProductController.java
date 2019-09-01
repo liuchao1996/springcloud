e09ce3556c4274708d0b036d2d3dbb2d6524ef50
1659419264@qq.com <147258gitee>
2019-09-01 20:30:53 +0800

    配置中心

--- product_service/src/main/java/com/lc/product_service/controller/ProductController.java
+++ product_service/src/main/java/com/lc/product_service/controller/ProductController.java
@@ -5,16 +5,21 @@ import com.lc.product_service.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${env}")
+    private String env;
 
     @Autowired
     private ProductService productService;
@@ -44,7 +49,7 @@ public class ProductController {
         Product product = productService.findById(id);
         Product result = new Product();
         BeanUtils.copyProperties(product, result);
-        result.setName(product.getName() + " data from port=" + port);
+        result.setName(product.getName() + " data from port=" + port + " env=" + env);
         return result;
     }
 
